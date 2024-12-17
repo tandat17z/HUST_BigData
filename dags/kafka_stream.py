01759a5ab237db0d23f6b0ed30be76674752f70a
tandat17zSonat <datlt@sonat.vn>
2024-12-17 20:28:33 +0700

    [Fix] run dag: fix db

--- dags/kafka_stream.py
+++ dags/kafka_stream.py
@@ -42,18 +42,34 @@ def format_data(res):
     return data
 
 def stream_data():
-    res = get_data()
-    res = format_data(res)
-    print(res)
+    import json
+    from kafka import KafkaProducer
+    import time
+    import logging
 
-with DAG('user_automation',
+    print("stream_data")
+    producer = KafkaProducer(bootstrap_servers=['broker:29092'], max_block_ms=5000)
+    curr_time = time.time()
+    while True:
+        if time.time() > curr_time + 60: #1 minute
+            break
+        try:
+            res = get_data()
+            res = format_data(res)
+
+            producer.send('users_created', json.dumps(res).encode('utf-8'))
+        except Exception as e:
+            logging.error(f'An error occured: {e}')
+            continue
+
+with DAG('kafka_stream_user_automation',
          default_args=default_args,
          description = "This is kafka stream task.",
-         schedule_interval='* * * * *',
+         schedule_interval='@daily',
          catchup=False) as dag:
 
     streaming_task = PythonOperator(
-        task_id='stream_data_from_api',
+        task_id='stream_data',
         python_callable=stream_data
     )
 
